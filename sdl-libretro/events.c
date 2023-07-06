2ff2d516ea7b98bb52803a7f2c505dbb51fddb32
宋文武 <iyzsong@member.fsf.org>
2023-07-06 19:04:00 +0800

    sdl: Handle joypad.

--- sdl-libretro/events.c
+++ sdl-libretro/events.c
@@ -16,6 +16,36 @@ static void PumpKeyboardEvents(void)
       SDL_PrivateKeyboard(state ? SDL_PRESSED : SDL_RELEASED, &sym);
     }
   }
+
+  static int16_t buttons[16] = {0};
+  static const int bkeys[16] = {
+    [RETRO_DEVICE_ID_JOYPAD_B] = SDLK_SPACE,
+    [RETRO_DEVICE_ID_JOYPAD_Y] = SDLK_RCTRL,
+    [RETRO_DEVICE_ID_JOYPAD_SELECT] = SDLK_0,
+    [RETRO_DEVICE_ID_JOYPAD_START] = SDLK_a,
+    [RETRO_DEVICE_ID_JOYPAD_UP] = SDLK_UP,
+    [RETRO_DEVICE_ID_JOYPAD_DOWN] = SDLK_DOWN,
+    [RETRO_DEVICE_ID_JOYPAD_LEFT] = SDLK_LEFT,
+    [RETRO_DEVICE_ID_JOYPAD_RIGHT] = SDLK_RIGHT,
+    [RETRO_DEVICE_ID_JOYPAD_A] = SDLK_RETURN,
+    [RETRO_DEVICE_ID_JOYPAD_X] = SDLK_ESCAPE,
+    [RETRO_DEVICE_ID_JOYPAD_L] = SDLK_o,
+    [RETRO_DEVICE_ID_JOYPAD_R] = SDLK_s,
+    [RETRO_DEVICE_ID_JOYPAD_L2] = SDLK_SPACE,
+    [RETRO_DEVICE_ID_JOYPAD_R2] = SDLK_RETURN,
+    [RETRO_DEVICE_ID_JOYPAD_L2] = 0,
+    [RETRO_DEVICE_ID_JOYPAD_R3] = 0,
+  };
+  for (int i = 0; i < 16; ++i) {
+    int16_t state = SDL_libretro_input_state_cb(0, RETRO_DEVICE_JOYPAD, 0, i);
+    int k = bkeys[i];
+    if (k && buttons[i] != state) {
+      buttons[i] = state;
+      sym.scancode = k;
+      sym.sym = k;
+      SDL_PrivateKeyboard(state ? SDL_PRESSED : SDL_RELEASED, &sym);
+    }
+  }
 }
 
 static Uint8 to_button(int16_t pressed)
